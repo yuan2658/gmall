4be25eb52d9adc6615ee27e81fa528a6fe6ea890
yuan2658 <yuan123.1>
2019-12-14 19:50:27 +0800

    add model

--- gmall-api/src/main/java/com/yuan/gmall/service/AttrService.java
+++ gmall-api/src/main/java/com/yuan/gmall/service/AttrService.java
@@ -1,10 +1,18 @@
 package com.yuan.gmall.service;
 
 import com.yuan.gmall.bean.PmsBaseAttrInfo;
+import com.yuan.gmall.bean.PmsBaseAttrValue;
+import com.yuan.gmall.bean.PmsBaseSaleAttr;
 
 import java.util.List;
 
 
 public interface AttrService {
     List<PmsBaseAttrInfo> attrInfoList(String catalog3Id);
+
+    String saveAttrInfo(PmsBaseAttrInfo pmsBaseAttrInfo);
+
+    List<PmsBaseAttrValue> getAttrValueList(String attrId);
+
+    List<PmsBaseSaleAttr> baseSaleAttrList();
 }
